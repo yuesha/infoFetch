c26b3062ab6700450cd8c930d6a473a90fa9f1b0
yuesha <2661103378@qq.com>
2019-11-15 23:43:00 +0800

    分类管理

---
+++ codeBehind/application/admin/controller/ClassifyManageController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\admin\controller;
+
+/**
+ * 分类管理控制器
+ */
+class ClassifyManageController extends \app\comm\baseClass\BaseController
+{
+    public function index()
+    {
+        return view();
+    }
+}
