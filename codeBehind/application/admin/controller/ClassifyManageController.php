332e43de8f6b196f8cfb047bdc4a9994dfbadae7
yuesha <2661103378@qq.com>
2019-11-16 12:42:31 +0800

    分类列表数据获取

--- codeBehind/application/admin/controller/ClassifyManageController.php
+++ codeBehind/application/admin/controller/ClassifyManageController.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\admin\controller;
 
+use app\comm\model\classifyModel as Classify;
+
 /**
  * 分类管理控制器
  */
@@ -9,5 +11,13 @@ class ClassifyManageController extends \app\comm\baseClass\BaseController
     public function index()
     {
         return view();
+    }
+    public function add()
+    {
+        return view();
+    }
+    public function getList($page = 1, $limit = 10)
+    {
+        return Classify::getList($page, $limit);
     }
 }
