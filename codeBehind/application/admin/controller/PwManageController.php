8333aeec3a0dde12917bad4c3774920b18711beb
unknown <1158138568@qq.com>
2019-11-16 09:58:52 +0800

    修改密码

--- codeBehind/application/admin/controller/PwManageController.php
+++ codeBehind/application/admin/controller/PwManageController.php
@@ -13,13 +13,19 @@ class PwManageController extends \app\comm\baseClass\BaseController
 
     public function save()
     {
+    	//接收表单的值
     	$data=input('post.');
     	$manage = session("manage");
+    	//请求数据库
     	$admin_info=$manage->where(name,$manage)->find();
+    	//如果接收到值
     	if($data){
+    		//判断原密码和新密码是否一致
     		if((md5($data['oldPw'])) == $admin_info['pw']){
+    			//判断新密码和重复密码是否一致
     			if($data['pw'] == $data['repass']){
     				$md5_pw=md5($data['pw']);
+    				//更新数据库
     				$result=manage::where(name,$manage)->update(['pw'=>$md5_pw]);
     				if($result){
     					exit_msg("密码修改成功");
