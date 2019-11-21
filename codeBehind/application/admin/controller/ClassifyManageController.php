1486af1ffbab09f6dd43456dda187fde29558e6b
yuesha <2661103378@qq.com>
2019-11-21 22:16:02 +0800

    分类添加（暂存）

--- codeBehind/application/admin/controller/ClassifyManageController.php
+++ codeBehind/application/admin/controller/ClassifyManageController.php
@@ -21,6 +21,21 @@ class ClassifyManageController extends \app\comm\baseClass\BaseController
     // 获取分类列表数据
     public function getList($page = 1, $limit = 10)
     {
+        // 使用模型里的方法去获取数据
         return Classify::getList($page, $limit);
+    }
+    // 保存分类信息
+    public function save()
+    {
+        // 接收分类信息
+        $name = trim_blank(input('post.name'));
+        $id = (int)input('post.id');
+        // 添加时判断分类是否存在
+        if (!$id) {
+            $isset = Classify::getByName($name);
+            if ($isset) {
+                exit_msg("此分类已存在，请勿重复添加");
+            }
+        }
     }
 }
