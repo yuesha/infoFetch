c76bd7b3c5eb541aba95194fcdf6da7ca6af4aff
yuesha <2661103378@qq.com>
2019-11-21 22:55:01 +0800

    添加分类功能完成

--- codeBehind/application/admin/controller/ClassifyManageController.php
+++ codeBehind/application/admin/controller/ClassifyManageController.php
@@ -2,6 +2,7 @@
 namespace app\admin\controller;
 
 use app\comm\model\classifyModel as Classify;
+use app\comm\model\manageLogModel as ManageLog;
 
 /**
  * 分类管理控制器
@@ -30,12 +31,24 @@ class ClassifyManageController extends \app\comm\baseClass\BaseController
         // 接收分类信息
         $name = trim_blank(input('post.name'));
         $id = (int)input('post.id');
-        // 添加时判断分类是否存在
         if (!$id) {
+            // 添加时判断分类是否存在
             $isset = Classify::getByName($name);
             if ($isset) {
                 exit_msg("此分类已存在，请勿重复添加");
             }
+            // 添加分类信息
+            $data['name'] = $name;
+            $data['add_user'] = session("manage") -> name;
+            $data['created_at'] = time();
+            Classify::create($data);
+            // 记录添加分类日志
+            ManageLog::log($name." 分类添加成功");
+            // 清除缓存
+            \think\Cache::clear();
+            exit_msg($name." 分类添加成功！",0);
+        }else{
+            // 修改时
         }
     }
 }
