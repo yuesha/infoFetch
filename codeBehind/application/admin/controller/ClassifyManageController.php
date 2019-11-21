5958f57ccb2773cd6250ae12efcd19c77455075e
yuesha <2661103378@qq.com>
2019-11-21 18:05:51 +0800

    补充分类控制器的注释

--- codeBehind/application/admin/controller/ClassifyManageController.php
+++ codeBehind/application/admin/controller/ClassifyManageController.php
@@ -8,14 +8,17 @@ use app\comm\model\classifyModel as Classify;
  */
 class ClassifyManageController extends \app\comm\baseClass\BaseController
 {
+    // 分类列表首页渲染
     public function index()
     {
         return view();
     }
+    // 分类添加首页渲染
     public function add()
     {
         return view();
     }
+    // 获取分类列表数据
     public function getList($page = 1, $limit = 10)
     {
         return Classify::getList($page, $limit);
