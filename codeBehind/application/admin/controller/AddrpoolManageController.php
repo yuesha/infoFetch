6d460af031001bbf758922f5afbd372e025462e2
yuesha <2661103378@qq.com>
2019-11-24 21:28:49 +0800

    地址池管理完成

--- codeBehind/application/admin/controller/AddrpoolManageController.php
+++ codeBehind/application/admin/controller/AddrpoolManageController.php
@@ -13,24 +13,30 @@ class AddrpoolManageController extends \app\comm\baseClass\BaseController
     // 地址池列表首页渲染
     public function index()
     {
-        $classify = Classify::all(null, '', true);
-        $data['classify'] = collection($classify)->toArray();
+        // 获取所有分类信息
+        $data['classify'] = Classify::all(null, '', true);
         return view('',$data);
     }
     // 地址池添加渲染
     public function add()
     {
         $data['session'] = session('manage') -> toArray();
+        $data['classify'] = Classify::all();
         return view('',$data);
     }
     // 地址池修改渲染
     public function edit($id)
     {
+        // 检测目标地址池是否有效
         $fetchPool = fetchPool::get($id);
         if (!$fetchPool) {
             exit_msg("未知错误");
         }
+        // 获取目标地址池信息
         $data['fetchPool']  = $fetchPool -> toArray();
+        // 获取所有分类信息
+        $data['classify']  = Classify::all(null, '', true);
+        // 获取管理员信息
         $data['session']  = session('manage') -> toArray();
         return view('',$data);
     }
@@ -45,25 +51,23 @@ class AddrpoolManageController extends \app\comm\baseClass\BaseController
     public function save()
     {
         // 接收地址池信息
-        $name = trim_blank(input('post.name'));
         $id = (int)input('post.id');
+        $data = $this -> checkData();
         if ($id === 0) {
             // 添加时判断地址池是否存在
-            $isset = fetchPool::getByName($name);
+            $isset = fetchPool::getByName($data['name']);
             if ($isset) {
                 exit_msg("此地址池已存在，请勿重复添加");
             }
             // 添加地址池信息
-            $data['name'] = $name;
-            $data['add_user'] = session("manage") -> name;
             $data['created_at'] = time();
             fetchPool::create($data);
             $sql = fetchPool::getLastsql();
             // 记录添加地址池日志
-            ManageLog::log($name." 地址池添加成功",$sql);
+            ManageLog::log($data['name']." 地址池添加成功",$sql);
             // 清除缓存
             \think\Cache::clear();
-            exit_msg($name." 地址池添加成功！",0);
+            exit_msg($data['name']." 地址池添加成功！",0);
         }else{
             // 修改时确定此地址池存在
             $fetchPool = fetchPool::get($id);
@@ -71,14 +75,38 @@ class AddrpoolManageController extends \app\comm\baseClass\BaseController
                 exit_msg("此地址池不存在");
             }
             $old_name = $fetchPool -> name;
-            $fetchPool -> name = $name;
-            $fetchPool -> save();
+            $fetchPool -> save($data,['id' => $id]);
             $sql = $fetchPool::getLastsql();
-            ManageLog::log("修改 ".$old_name." 地址池名称为： ".$name,$sql);
+            ManageLog::log("修改 ".$old_name." 地址池信息成功",$sql);
             // 清除缓存
             \think\Cache::clear();
             exit_msg("修改成功！",0);
         }
+    }
+    // 数据验证
+    private function checkData()
+    {
+        $initData = input('post.');
+        $data['name'] = trim_blank($initData['name']);
+        $data['auto_url'] = trim_blank($initData['auto_url']);
+        foreach ($initData['auto_rule'] as $k1 => $v1) {
+            if (empty($v1)) {
+                unset($initData['auto_rule'][$k1]);
+            }
+        }
+        foreach ($initData['default_rule'] as $k2 => $v2) {
+            if (empty($v2)) {
+                unset($initData['default_rule'][$k2]);
+            }
+        }
+        $data['auto_rule'] = json_encode($initData['auto_rule']);
+        $data['default_rule'] = json_encode($initData['default_rule']);
+        // 如果自动抓取标识位未选中，则转换为0
+        $initData['is_auto'] = isset($initData['is_auto'])?$initData['is_auto']:0;
+        $data['is_auto'] = (int)$initData['is_auto'];
+        $data['classify_id'] = (int)$initData['classify_id'];
+        // halt($data);
+        return $data;
     }
     // 删除地址池信息
     public function del()
--- codeBehind/public/static/behind/addrpool_manage_add.js
+++ codeBehind/public/static/behind/addrpool_manage_add.js
@@ -1,30 +1,39 @@
-$(".add-span").click(function(){
+
+// 插入 新的元素
+function add_span(rule,obj){
 	// 即将创建的内容
-	var text = $(this).parents().prev().children("input").val();
+	var text = $(obj).parents().prev().children("input").val();
 	if (text == "") {
 		layer.msg("输入框不能为空！",{icon: 2});
 	}else{
 		// 创建节点内容
 		var str = '<div class="layui-form-item del-default-rules">\
-			<label for="default_rule" class="layui-form-label">\
+			<label for="' + rule + '" class="layui-form-label">\
 				<span class="x-red"></span>                       \
 			</label>\
 			<div class="layui-input-inline">\
-				<input type="text" id="default_rule" name="default_rule" lay-verify="" autocomplete="off" class="layui-input add-input" value="' + text + '"/>\
+				<input type="text" id="' + rule + '" name="' + rule + '[]" lay-verify="" autocomplete="off" class="layui-input add-input" value="' + text + '"/>\
 			</div>\
 			<div class="layui-form-mid layui-word-aux">\
-				<span class="layui-badge layui-bg-green layuiadmin-badge del-span" onclick="del_span(this)">x</span>\
+				<span class="layui-badge layui-bg-red layuiadmin-badge del-span" onclick="del_span(this)">x</span>\
 			</div>\
 		</div>';
 		// 即将创建的节点
 		var $div = $(str);
 		// 将节点插入DOM树中
-		$(this).parents('.layui-form-item').append($div);
+		$(obj).parents('.layui-form-item').append($div);
+		// 创建的输入框只读不可修改
+		$('.add-input').attr('readonly','readonly');
 		// 清空原先的输入框
-		$(this).parents().prev().children("input").val('')
+		$(obj).parents().prev().children("input").val('')
 	};
-});
+}
+// 删除 添加的元素
 function del_span(obj){
 	var obj2 = $(obj).parents('.del-default-rules');
 	obj2.remove();
 };
+// 编辑-删除原有的元素
+function delThis(obj) {
+	$(obj).parents('div.demo').remove();
+}
