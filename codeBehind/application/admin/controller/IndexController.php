f950fe55d9f8b09490d9e18616eadf4f6ac21cc1
yuesha <2661103378@qq.com>
2019-11-26 20:24:16 +0800

    修改欢迎页面的数据统计，分类数量和访问次数

--- codeBehind/application/admin/controller/IndexController.php
+++ codeBehind/application/admin/controller/IndexController.php
@@ -1,6 +1,8 @@
 <?php
-
 namespace app\admin\controller;
+use app\comm\model\manageLogModel as ManageLog;
+use app\comm\model\classifyModel as Classify;
+
 /**
  * 后台首页控制器
  */
@@ -19,6 +21,8 @@ class IndexController extends \app\comm\baseClass\BaseController
     	$manage = session("manage");
     	// 需要传递给模型的数据
     	$data['manage'] = $manage -> toArray();
+        $data['visitTimes'] = ManageLog::where(['action' => '登录成功']) -> count();
+        $data['classifyTimes'] = Classify::where('') -> count();
         return view('',$data);
     }
 }
