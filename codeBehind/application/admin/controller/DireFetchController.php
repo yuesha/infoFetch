aac5965e0d58f609b16ff7c7f2fd052d5af6faa8
yuesha <2661103378@qq.com>
2019-11-29 12:24:00 +0800

    从地址池的目录处根据抓取规则抓取地址-初始渲染

--- codeBehind/application/admin/controller/DireFetchController.php
+++ codeBehind/application/admin/controller/DireFetchController.php
@@ -20,7 +20,20 @@ class DireFetchController extends \app\comm\baseClass\BaseController
     // 获取 符合自动抓取地址条件的 地址池列表数据
     public function getList($page = 1, $limit = 10)
     {
+        // 获取所有数据
+        $allList = fetchPool::all(function($query){
+            $query -> where('is_auto = 1') -> where('auto_rule <> "[]"') -> where('auto_url IS NOT NULL') -> where('auto_url <> ""');
+        },'',true);
+        $allList = collection($allList) -> toArray();
+        // 获取符合条件的数据（分页）(含缓存)
+        // use 可以将外部变量拷贝进入闭包函数内
+        $list = fetchPool::all(function ($query) use ($page, $limit) {
+            $query -> where('is_auto = 1') -> where('auto_rule <> "[]"') -> where('auto_url IS NOT NULL') -> where('auto_url <> ""') -> limit(($page - 1) * $limit, $limit)->order('id', 'asc');
+        }, '', true);
+        // 将返回的 list 对象转换为数组
+        $list = collection($list)->toArray();
 
-        // return return_table();
+        // 返回数据
+        return return_table($list,$allList);
     }
 }
