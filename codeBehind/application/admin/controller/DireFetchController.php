ac0e9ed003180557c853ec3badb5aed4daa7f2e6
yuesha <2661103378@qq.com>
2020-02-01 21:15:20 +0800

    抓取目录地址初步

--- codeBehind/application/admin/controller/DireFetchController.php
+++ codeBehind/application/admin/controller/DireFetchController.php
@@ -2,8 +2,10 @@
 namespace app\admin\controller;
 
 use app\comm\model\fetchPoolModel as fetchPool;
+use app\comm\model\fetchUrlModel as fetchUrl;
 use app\comm\model\classifyModel as Classify;
 use app\comm\model\manageLogModel as ManageLog;
+use QL\QueryList;
 
 /**
  * 从目录抓取地址 控制器
@@ -39,8 +41,56 @@ class DireFetchController extends \app\comm\baseClass\BaseController
     }
 
     // 从目录抓取地址信息
-    public function catch(int $id)
+    public function catch()
     {
-        # code...
+        $redis = new \redis();
+        $redis->connect('127.0.0.1', 6379);
+        $redis->auth('121213');
+        // 获取地址池id
+        $id = (int)input('post.id');
+        $fetchPoolRow = $this -> checkPool($id);
+
+        $url = parse_url($fetchPoolRow -> auto_url);
+        echo $url['host'];exit;
+        $rule = "#list dl dd a";
+        $rules = [
+            // 采集章节名称
+            'title' => ['#list dl dd a','text'],
+            // 采集章节地址
+            'href' => ['#list dl dd a','href'],
+        ];
+        $data = QueryList::get($url)->rules($rules)->query()->getData();
+        $data = $data -> all();
+
+        // 清除此地址池当中的所有地址
+        fetchUrl::where(['fetch_pool_id' => $id]) -> delete();
+        $insData['fetch_pool_id'] = $id;
+        $insData['is_fetch'] = 1;
+        $insData['rule'] = '';
+        $insData['add_user'] = session('manage') -> name;
+        foreach ($data as $urlRow) {
+            $insData['title'] = $urlRow['title'];
+            $insData['url'] = $urlRow['href'];
+            $insData['created_at'] = time();
+            fetchUrl::create($insData);
+            // $redis->lpush("demo1", $insData['title']);
+        }
+        exit_msg("抓取成功",0);
+    }
+
+    // 检查地址池信息
+    private function checkPool(int $id = 0)
+    {
+        if (!$id) {
+            exit_msg("地址池信息错误！",1);
+        }
+        $fetchPoolRow = fetchPool::get($id);
+        if ($fetchPoolRow == NULL) {
+            exit_msg("地址池信息错误！",2);
+        }
+        if ($fetchPoolRow -> auto_rule == []) {
+            exit_msg("地址池不存在自动抓取规则！",3);
+        }
+        return $fetchPoolRow;
     }
 }
---
+++ codeBehind/application/comm/baseClass/RedisController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\comm\baseClass;
+
+/**
+ * redis 缓存操作类
+ */
+class RedisController extends \think\Controller
+{
+    // 继承时自动执行
+    function __construct()
+    {
+    }
+}
